662cd0fc11157498260e5b629f88adc12b13aa5a
Julian Chamale <jchamale.usac@gmail.com>
2024-10-09 10:28:31 -0600

    feat: add api to gitignore

--- apps/threads-api/webpack.config.js
+++ apps/threads-api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/threads-api'),
+    path: join(__dirname, '../../api/apps/threads-api'),
     filename: 'api/index.js',
 
     chunkFormat: 'module',
